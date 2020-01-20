685fcca2b9caf910764ff2dbd2a58c8f0bee8a81
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2020-01-19 21:46:06 -0800

    moved html files into one folder with assets

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 const express = require("express");
-const remove = require("./routes/deleteNote");
-const html = require('./routes/html');
-const open = require('./routes/apiNote');
-const save = require('./routes/saveNote');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + "/index.html"));
-app.use(express.static(__dirname + "/notes.html"));
+app.use(express.static(__dirname + "/webpage"));
 
-require("./routes/deleteNote")(rem)
-require("./routes/html")(app)
-require("./routes/apiNote")(open)
-require("./routes/saveNote")(save)
+
+require("./routes/apiNote")(app)
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
