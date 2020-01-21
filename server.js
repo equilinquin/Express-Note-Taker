9527bf8f3ad208de3ad9af7eaaa00c1f85fbcd7b
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2020-01-21 15:32:01 -0800

    added to server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-const PORT = 1000;
-
+const PORT = process.env.PORT || 1000;
+ 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/webpage"));
